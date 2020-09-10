746386285db39f4fc6e204ecc075d4cfcad5103f
eyawson <ebow.yawson@gmail.com>
2020-09-10 03:46:38 -0400

    added dockerpath to enable push to dockerhub

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=predict .
+docker build --tag=housingprediction .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 predict
+docker run -p 8000:80 housingprediction
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,6 +6,7 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=akwele/housingprediction
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +14,4 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push akwele/housingprediction:latest
