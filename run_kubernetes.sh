2700dea438ccf5f0596eaedbe70d6f24265aa3b4
eyawson <ebow.yawson@gmail.com>
2020-09-10 04:14:55 -0400

    added kubernetes run configuration to run_kubernetes.txt

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,21 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=akwele/housingprediction
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deployment --image=$dockerpath ml-housing-predictor
 
+kubectl run ml-housing-predictor\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=ml-housing-predictor
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-housing-predictor 8000:80
